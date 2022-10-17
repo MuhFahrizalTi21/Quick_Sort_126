fceadbac4236e87c1f3344cc19076fe9bb7feda4
Muhammad  Fahrizal <m.fahrizal.ft21@mail.umy.ac.id>
2022-10-17 17:46:57 +0700

    membuat void display

--- Quick_Sort_126/Program.cs
+++ Quick_Sort_126/Program.cs
@@ -104,6 +104,19 @@ namespace Quick_Sort_126
             //sort the list on the right of pivot using quick sort
             q_sort(j+1, high);
         }
+        void display()
+        {
+            Console.WriteLine("\n -----------------------------");
+            Console.WriteLine("\n Sorted array elements");
+            Console.WriteLine("\n -----------------------------");
+
+            for (int j =0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]);
+            }
+            Console.WriteLine("\n Number of comparisons : " + cmp_count);
+            Console.WriteLine("\n Number of data movements : " + mov_count);
+        }
         static void Main(string[] args)
         {
         }
